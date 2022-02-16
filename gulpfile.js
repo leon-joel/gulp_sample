8bef6601aec4d3a53d53bb6ba6c7af69e7e1c0e6
Leon <leopard10000@gmail.com>
2022-02-16 10:25:07 +0900

    watchを使ったscssファイル監視

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require('gulp'); // gulpを読み込む
-var uglify = require('gulp-uglify'); // gulp-uglifyを読み込む
+const uglify = require('gulp-uglify'); // gulp-uglifyを読み込む
 
 // 「uglify」タスクを定義する
 gulp.task('uglify', function () {
@@ -14,7 +14,7 @@ gulp.task('uglify', function () {
 });
 
 gulp.task('task1', function () {
-  return consolo.log('hello');
+  return console.log('hello');
 });
 
 // gulp.task('task2', ['task1'], function () {        // ←3系での書き方。4系以降だとエラーになる
@@ -22,15 +22,30 @@ gulp.task('task2', gulp.task('task1'), function () {
   console.log('world');
 });
 
-gulp.task('default', gulp.task('task2'));
+gulp.task('task3', gulp.task('task2'));
 
 
 // var sass = require("gulp-sass");                   // gulp-sass単体にはsassコンパイラーが含まれなくなったため、"sass"をインストールして、↓の書き方にする必要がある
 const sass = require('gulp-sass')(require('sass'));
 
+// ■sassからcssをビルドする
 gulp.task("sass", function (done) {
   gulp.src("./src/sass/**/*.scss")
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
     .pipe(gulp.dest("./dist/css"));
   done();                                             // callbackを呼び出さないとdid not complete になるらしい 参考: https://qiita.com/baby-0105/items/aa09ebb9232b6f740281
+});
+
+// ■watchを使う ※gulp で開始、ctrl+C で停止
+gulp.task("default", function() {
+  return gulp.watch("src/sass/**/*.scss", function() {
+    return (
+      gulp
+        .src("src/sass/**/*.scss")
+        .pipe(
+          sass({outputStyle: "expanded"}).on("error", sass.logError)
+        )
+        .pipe(gulp.dest("dist/css"))
+    );
+  })
 });
